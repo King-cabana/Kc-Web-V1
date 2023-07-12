f076772ebcb9134d3fe68f99c6bc52df45277938
Josiah ENUMAH <josiahenumah94@gmail.com>
2023-07-12 11:08:26 +0100

    updates to navbar and absolute footer of previous-event in organizer profile page view

--- src/userFlows/eventOrganizerProfile/previousEvents/PreviousEvents.js
+++ src/userFlows/eventOrganizerProfile/previousEvents/PreviousEvents.js
@@ -2,17 +2,23 @@ import { useNavigate } from "react-router-dom";
 import FooterButtonComponent from "../../../components/FooterButtonComponent";
 import { GuestTopBar } from "../../createProposal/viewProposal/ShareViewProposal";
 import SingleEventHistory from "../../eventHistory/components/SingleEventHistory";
+import TopBar from "../../topBar/dashboardTopBar/TopBar";
+import { useSelector } from "react-redux";
+import { encryptId } from "../../../utils";
+import { Div } from "../../../globalStyles";
 
 const PreviousEvents = () => {
   const navigate = useNavigate();
-  const handleContact = ()=>navigate("/signin")
   // const state = useSelector((state)=>state?.pastEvent)
+  const userDetails = useSelector((state)=>state?.userDetails)
+  const profile = useSelector((state)=>state?.eventOrganizerProfile);
+  const handleContact = ()=>navigate(`/organizer-profile/${encryptId(profile?.id)}`)
   return (
     <>
       <SingleEventHistory
-        topBar={<GuestTopBar />}
+        topBar={userDetails?.isSignedIn === true ? (<div style={{marginBottom: "5rem"}}><TopBar/></div>)  : <GuestTopBar />}
         footerButtonComponent={
-          <FooterButtonComponent whiteBtnText="Contact" onClickWhiteBtn={handleContact} marginright="0rem" />
+          <FooterButtonComponent whiteBtnText="Back" onClickWhiteBtn={handleContact} marginright="0rem" />
         }
       />
     </>
